531bbfae0cbddab9a7ff45d3473ffb6e5eb90b5e
Jonas Bernoulli <jonas@bernoul.li>
2022-02-11 21:39:54 +0100

    Fix typo in commentary

--- sisyphus.el
+++ sisyphus.el
@@ -25,7 +25,7 @@
 
 ;;; Commentary:
 
-;; Create a release and watch it role down the hill again.
+;; Create a release and watch it roll down the hill again.
 
 ;; Recommended setup:
 ;;   (with-eval-after-load 'magit (require 'sisyphus))
