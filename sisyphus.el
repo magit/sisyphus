af0ef7111e048e62bca0f1be1062fd7410e642d7
Jonas Bernoulli <jonas@bernoul.li>
2022-04-26 22:03:43 +0200

    Refresh library header

--- sisyphus.el
+++ sisyphus.el
@@ -1,19 +1,20 @@
-;;; sisyphus.el --- Create Elisp releases          -*- lexical-binding: t -*-
+;;; sisyphus.el --- Create Elisp releases -*- lexical-binding:t -*-
 
-;; Copyright (C) 2022  Jonas Bernoulli
+;; Copyright (C) 2022 Jonas Bernoulli
 
 ;; Author: Jonas Bernoulli <jonas@bernoul.li>
 ;; Homepage: https://github.com/magit/sisyphus
 ;; Keywords: git tools vc
 
-;; Package-Requires: ((emacs "27") (magit "3.4.0"))
 ;; Package-Version: 0.0.0-git
+;; Package-Requires: ((emacs "27") (magit "3.4.0"))
+
 ;; SPDX-License-Identifier: GPL-3.0-or-later
 
-;; This file is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation; either version 3, or (at your option)
-;; any later version.
+;; This file is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published
+;; by the Free Software Foundation, either version 3 of the License,
+;; or (at your option) any later version.
 ;;
 ;; This file is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -21,7 +22,7 @@
 ;; GNU General Public License for more details.
 ;;
 ;; You should have received a copy of the GNU General Public License
-;; along with Magit.  If not, see http://www.gnu.org/licenses.
+;; along with this file.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
